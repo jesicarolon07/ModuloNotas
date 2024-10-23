bd558338d7a5aa6a0f21e114e9b0dfd87ea623e0
Jesica Rolon <yesicarolon07@gmail.com>
2024-10-22 23:01:47 -0300

    Realizar funcionalidad intento 3  del boton modificar

--- modificar_nota/modificar_nota.php
+++ modificar_nota/modificar_nota.php
@@ -10,10 +10,16 @@
 
  
 <?php
+$id_nota = $_GET ['id_nota'];
+$query = "SELECT * FROM notas WHERE id_nota = $id_nota";
+$result = mysqli_query($conn, $query);
+$nota= mysqli_fetch_assoc($result);
 if($_SERVER["REQUEST_METHOD" == "POST"]){
     $id_nota= $_POST["id_nota"];
     $anio = $_POST["anio"];
     $nota= $_POST["nota"];
+    $tipo_nota= $_POST["tipo_nota"];
+    $periodo=$_POST["periodo"];
 
 }
 $query_modificar = "UPDATE notas SET anio='$anio', nota='$nota', tipo_nota='$tipo_nota', periodo='$periodo' WHERE id_nota=$id_nota";
@@ -24,11 +30,4 @@ if($conn->query($query_modificar) === TRUE){
 }
 
 
-
-echo "<div class='modal fade' id='editModal" . $row["id_nota"] . "' tabindex='-1' aria-labelledby='editModalLabel" . $row["id_nota"] . "' aria-hidden='true'>";
-echo "<form method='post' action=''>";
-echo "<input type='hidden' name='id_nota' value='" . $row["id_nota"] . "'>";
-echo "<input type='text' class='form-control' id='anio" . $row["id_nota"] . "' name='anio' value='" . $row["anio"] . "'>";
-echo "/form";
-echo "/div";
 ?>
